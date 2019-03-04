24efca4d5471cd44e87e596f4e82af367c31eb08
moskwam <marcin.moskwa96@gmail.com>
2019-03-04 20:57:31 +0100

    First commit.

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,7 +1,7 @@
 function submitComment(commentControl) {
     var comments = document.getElementsByName(commentControl)[0].value;
-    var subscriptionKey = "TEXT_ANALYTICS_API_KEY";
-    var url = "TEXT_ANALYTICS_ENDPOINT/sentiment";
+    var subscriptionKey = "45261365eb7f45218c7b4f4d0955f25f";
+    var url = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0";
 
     var payload = '{ "documents": [ { "language": "en-US", "id": "1", "text": "' + comments + '" }]}';
 
